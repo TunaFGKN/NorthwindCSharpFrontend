0e408fa70c05a9248d13370ca96dc7604a2cca1b
Tuna Figankaplan <swaxztn@gmail.com>
2021-03-11 00:05:38 +0300

    11.03.2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'northwind';
+  title: string = 'Northwind';
+  user: string = "Tuna Figankaplan";
+  
 }
+
