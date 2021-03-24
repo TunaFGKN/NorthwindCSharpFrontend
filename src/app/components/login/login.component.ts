d3ecd1cb3c7e19ff3e6a0e3e845ccca9c2b62422
TunaFigankaplan <swaxztn@gmail.com>
2021-03-24 23:04:24 +0300

    24.03.2021

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from "@angular/forms";
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +11,7 @@ import { FormGroup, FormBuilder, FormControl, Validators } from "@angular/forms"
 export class LoginComponent implements OnInit {
 
   loginForm:FormGroup
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(private formBuilder:FormBuilder, private authService:AuthService, private toastrService:ToastrService) { }
 
   ngOnInit(): void {
     this.createLoginForm()
@@ -25,6 +27,15 @@ export class LoginComponent implements OnInit {
   login(){
     if (this.loginForm.valid) {
       console.log(this.loginForm.value)
+      let loginModel = Object.assign({},this.loginForm.value)
+      this.authService.login(loginModel).subscribe(response=>{
+        this.toastrService.info(response.message,"Success")
+        localStorage.setItem("token",response.data.token)
+        console.log(response)
+      },responseError=>{
+        console.log(responseError)
+        this.toastrService.error(responseError.error,"Fail")
+      })
     }
   }
 }
